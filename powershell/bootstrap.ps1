006a445cc88b2fb1500443ecbcbebc381c960709
warman <warman@runreal.dev>
2024-04-01 22:13:37 -0400

    feat: add more options to ps installer

--- powershell/bootstrap.ps1
+++ powershell/bootstrap.ps1
@@ -1,16 +1,21 @@
-# < >
-$BootstrapFile = ".bootstrap-version"
-$BootstrapFilePath = Join-Path -Path $PSScriptRoot -ChildPath $BootstrapFile
+<# 
 
-function LoadPackageList {
-    $packageFile = Join-Path -Path $PSScriptRoot -ChildPath "winget-packages.json"
-    if (Test-Path $packageFile) {
-        $packages = (Get-Content $packageFile | ConvertFrom-Json).packages
-        Log "Loaded package list from configuration file"
+#>
+
+function Log($message, $type = "Info") {
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logMessage = "[$timestamp] [$type] $message"
+    switch ($type) {
+        "Error" { Write-Host $logMessage -ForegroundColor Red }
+        "Warning" { Write-Host $logMessage -ForegroundColor Yellow }
+        default { Write-Host $logMessage -ForegroundColor Green }
     }
-    else {
-        Log "Package configuration file not found. Exiting..." -type "Error"
-        exit
+    $logPath = Join-Path -Path $PSScriptRoot -ChildPath "bootstrap.log"
+    try {
+        Add-Content -Path $logPath -Value $logMessage
+    }
+    catch {
+        Write-Host "Failed to write to log file: $_" -ForegroundColor Red
     }
 }
 
@@ -32,66 +37,93 @@ function LoadEnvVariables {
     }
 }
 
-function Log($message, $type = "Info") {
-    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    $logMessage = "[$timestamp] [$type] $message"
-    switch ($type) {
-        "Error" { Write-Host $logMessage -ForegroundColor Red }
-        "Warning" { Write-Host $logMessage -ForegroundColor Yellow }
-        default { Write-Host $logMessage -ForegroundColor Green }
+function LoadPackageList {
+    $packageFile = Join-Path -Path $PSScriptRoot -ChildPath "winget-packages.json"
+    if (Test-Path $packageFile) {
+        $global:packages = (Get-Content $packageFile | ConvertFrom-Json).packages
+        Log "Loaded package list from configuration file"
     }
-    $logPath = Join-Path -Path $PSScriptRoot -ChildPath "bootstrap.log"
-    try {
-        Add-Content -Path $logPath -Value $logMessage
-    } catch {
-        Write-Host "Failed to write to log file: $_" -ForegroundColor Red
+    else {
+        Log "Package configuration file not found. Exiting..." -type "Error"
+        exit
     }
 }
 
-# Prompt the user for admin privileges
-$isAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
-
-if (-not $isAdmin) {
-    # Relaunch the script with admin privileges
-    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-    Exit
+function New-TemporaryFile {
+    $tempPath = [System.IO.Path]::GetTempPath()
+    $tempFile = [System.IO.Path]::Combine($tempPath, [System.IO.Path]::GetRandomFileName())
+    $null = New-Item -Path $tempFile -ItemType File -Force
+    return $tempFile
 }
 
-# Continue running the script with admin privileges
-Log "Running with admin privileges"
-
-function GetScriptHash {
-    $scriptHash = Get-FileHash $PSCommandPath -Algorithm SHA256
-    # Write-Host "Script hash: $($scriptHash.Hash)"
-    return $scriptHash.Hash
+function CheckAdmin {
+    if (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        return $true
+    }
+    return $false
 }
 
-function CheckWinGet {
+function CheckWinget {
+    Log "Checking winget version..."
     try {
         $wingetVersion = winget --version
         if ($wingetVersion) {
             return $wingetVersion
-        } else {
+        }
+        else {
             return $false
         }
-    } catch {
+    }
+    catch {
         return $false
     }
 }
 
-function InstallWinGet {
-    
-    Invoke-WebRequest -Uri https://aka.ms/winget-cli -OutFile winget-cli.msixbundle
-    Add-AppPackage .\winget-cli.msixbundle -Verbose
+function InstallWinget {
+    try {
+        Log "Downloading winget dependencies..."
+
+        # Download VCLibs
+        $VCLibs_Path = New-TemporaryFile
+        $VCLibs_Url = "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
+        Log "Downloading VCLibs from $VCLibs_Url to $VCLibs_Path`n`n"
+        Invoke-WebRequest -Uri $VCLibs_Url -OutFile $VCLibs_Path
+
+        # Download UI.Xaml
+        $UIXaml_Path = New-TemporaryFile
+        $UIXaml_Url = "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx"
+        Log "Downloading UI.Xaml from $UIXaml_Url to $UIXaml_Path`n"
+        Invoke-WebRequest -Uri $UIXaml_Url -OutFile $UIXaml_Path
+
+        # Download winget license
+        $winget_license_path = New-TemporaryFile
+        $winget_license_url = Get-WingetDownloadUrl -Match "License1.xml"
+        Log "Downloading winget license from $winget_license_url to $winget_license_path`n`n"
+        Invoke-WebRequest -Uri $winget_license_url -OutFile $winget_license_path
+
+        # Install everything
+        Log "Installing winget and its dependencies..."
+        Add-AppxProvisionedPackage -Online -PackagePath $winget_path -DependencyPackagePath $UIXaml_Path, $VCLibs_Path -LicensePath $winget_license_path | Out-Null
+
+        # Remove temp files
+        Remove-Item $VCLibs_Path
+        Remove-Item $UIXaml_Path
+        Remove-Item $winget_path
+        Remove-Item $winget_license_path
+    }
+    catch {
+        Log "Failed to install winget. Error: $_" -type "Error"
+    }
 }
 
 function InstallDeps {
-    Log "Installing dependencies"
+    Log "Installing dependencies..."
     foreach ($package in $packages) {
         try {
             winget install -e --id $package -h
             Log "$package installation attempted."
-        } catch {
+        }
+        catch {
             Log "Failed to install $package. Error: $_" -type "Error"
         }
     }
@@ -99,16 +131,24 @@ function InstallDeps {
 }
 
 function InstallVisualStudio {
-    Log "Installing VS"
     $vsconfigFile = Join-Path -Path $PSScriptRoot -ChildPath ".vsconfig"
     winget install --source winget --exact --id Microsoft.VisualStudio.2022.Professional --override "--passive --config $vsconfigFile"
 }
 
+function InstallVisualStudioCode {
+    Log "Installing VS Code..."
+    winget install Microsoft.VisualStudioCode --override '/SILENT /mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders"'
+}
+
 function InstallBuildkiteAgent {
+    Log "Installing buildkite-agent..."
+    $env:buildkiteAgentToken = $env:BUILDKITE_AGENT_TOKEN
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/buildkite/agent/main/install.ps1'))
 }
 
-function Add7ZipToPath {
+function Install7Zip {
+    Log "Installing 7-Zip..."
+    winget install -e --id 7zip.7zip -h
     if (!(Get-Command 7z -ErrorAction SilentlyContinue)) {
         $installDir = Join-Path -Path $env:ProgramFiles -ChildPath '7-Zip'
         $envMachinePath = [System.Environment]::GetEnvironmentVariable('PATH', 'machine')
@@ -118,31 +158,57 @@ function Add7ZipToPath {
     }
 }
 
+function ExitWithDelay {
+    param (
+        [int]$DelayInSeconds = 5,
+        [int]$ExitCode = 0
+    )
+    Start-Sleep -Seconds $DelayInSeconds
+    exit $ExitCode
+}
 function ParseArguments {
     param (
+        [Parameter(Mandatory = $false, Position = 0)]
         [string[]]$args
     )
 
     $global:installOptions = @{
-        "winget" = $args -contains "-winget"
-        "deps" = $args -contains "-deps"
-        "vs" = $args -contains "-vs"
-        "buildkite" = $args -contains "-buildkite"
-        "7zip" = $args -contains "-7zip"
-        "all" = $args -contains "-all"
+        "winget"    = $false
+        "deps"      = $false
+        "vs"        = $false
+        "buildkite" = $false
+        "7zip"      = $false
+        "all"       = $false
     }
 
-    if (-not $global:installOptions.Values -contains $true) {
+    foreach ($arg in $args) {
+        switch ($arg) {
+            "-winget" { $global:installOptions["winget"] = $true }
+            "-deps" { $global:installOptions["deps"] = $true }
+            "-vs" { $global:installOptions["vs"] = $true }
+            "-buildkite" { $global:installOptions["buildkite"] = $true }
+            "-7zip" { $global:installOptions["7zip"] = $true }
+            "-all" { $global:installOptions["all"] = $true }
+            default { Write-Host "Unknown option: $arg" -ForegroundColor Red }
+        }
+    }
+
+    if (-not ($global:installOptions.Values -contains $true)) {
         $global:installOptions["all"] = $true
     }
 }
 
 function Bootstrap {
     Log "Running bootstrap"
-    LoadEnvVariables
-    CheckWinGet
     if ($global:installOptions["winget"] -or $global:installOptions["all"]) {
-        InstallDeps
+        $wingetVersion = CheckWinget
+        if (-not $wingetVersion) {
+            InstallWinget
+        }
+    }
+    if (-not (CheckWinget)) {
+        Log "Winget not found. Please install winget and try again." -type "Error"
+        ExitWithDelay 1
     }
     if ($global:installOptions["deps"] -or $global:installOptions["all"]) {
         InstallDeps
@@ -151,34 +217,32 @@ function Bootstrap {
         InstallBuildkiteAgent
     }
     if ($global:installOptions["7zip"] -or $global:installOptions["all"]) {
-        winget install -e --id 7zip.7zip -h
-        Add7ZipToPath
+        Install7Zip
     }
     if ($global:installOptions["vs"] -or $global:installOptions["all"]) {
+        InstallVisualStudioCode
         InstallVisualStudio
     }
     Log "Bootstrap complete"
 }
 
-$ScriptHash = GetScriptHash
-if ( -Not (Test-Path $BootstrapFilePath.Trim() )) {
-    Log "Detected first run - running bootstrap"
+# ============================================================================ #
+# Setup
+# ============================================================================ #
+if (-not (CheckAdmin)) {
+    Log "Please run this script as an administrator." -type "Error"
+    ExitWithDelay 1
+}
+
+# ============================================================================ #
+# Main Script
+# ============================================================================ #
+try {
+    LoadEnvVariables
     LoadPackageList
     ParseArguments $args
     Bootstrap
-    New-Item -path $PSScriptRoot -name ".bootstrap-version" -type "file" -value $ScriptHash
 }
-else {
-    $text = Get-Content -Path $BootstrapFilePath -First 1
-    if ( $text -eq $ScriptHash) {
-        Log "Bootstrap is up to date - Skipping"
-    }
-    else {
-        Log "Bootstrap is out of date - running bootstrap"
-        Bootstrap
-        Set-Content -Path $BootstrapFilePath -value $ScriptHash
-    }
+catch {
+    Log "An error occurred: $_" -type "Error"
 }
-
-Log "Script completed. Press any key to exit..."
-$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
